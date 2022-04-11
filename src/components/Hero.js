67c81a2693e7f6cc875fb1c4a5fcebe5e2e5dc59
ricky <ricky.chaggar@gmail.com>
2022-04-11 16:14:42 +0100

    Change hero to use display: flex for positioning images and text

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,15 +4,13 @@ import photo_grid from "../images/photo-grid.png";
 function Hero() {
   return (
     <div className="hero">
-      <div className="hero__image__container">
-        <img src={photo_grid} alt="" className="hero__image" />
-      </div>
-      <div className="hero__text__container">
+      <img src={photo_grid} alt="" className="hero__image" />
+      <div className="hero__text">
         <h1 className="hero__title">Online Experiences</h1>
-        <h4 className="hero__text">
+        <p className="hero__content">
           Join unique interactive activities led by one-of-a-kind hosts—all
           without leaving home.
-        </h4>
+        </p>
       </div>
     </div>
   );

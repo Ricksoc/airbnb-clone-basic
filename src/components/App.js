5b851a304b75ead2dedec93347f5dd2d68b22552
ricky <ricky.chaggar@gmail.com>
2022-04-12 15:22:26 +0100

    Set up activity card using hard coded content. Single activity card creasted.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Hero from "./Hero";
+import Card from "./Card";
 
 function App() {
   return (
     <div className="card">
       <Navbar />
       <Hero />
+      <Card />
     </div>
   );
 }
